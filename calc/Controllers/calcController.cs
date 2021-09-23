45b0437da5a5ed73786426ce82a47c2c85e29e4e
xjxiaosg <xjxiao@simtech.a-star.edu.sg>
2021-09-23 10:13:15 +0800

    Add project files.

---
+++ calc/Controllers/calcController.cs
@@ -0,0 +1,42 @@
+﻿using calc.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace calc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class calcController : ControllerBase
+    {
+        Calc oCalc = new Calc();
+
+        [HttpGet("add/{paramOne}/{paramTwo}")]
+        public float Get(float paramOne, float paramTwo)
+        {
+            return oCalc.Add(paramOne, paramTwo);
+        }
+
+        [HttpGet("sub/{paramOne}/{paramTwo}")]
+        public float GetSub(float paramOne, float paramTwo)
+        {
+            return oCalc.Sub(paramOne, paramTwo);
+        }
+
+        [HttpGet("mul/{paramOne}/{paramTwo}")]
+        public float GetMul(float paramOne, float paramTwo)
+        {
+            return oCalc.Mul(paramOne, paramTwo);
+        }
+
+        [HttpGet("div/{paramOne}/{paramTwo}")]
+        public float GetDiv(float paramOne, float paramTwo)
+        {
+            return oCalc.Div(paramOne, paramTwo);
+        }
+    }
+}
---
+++ calc/Model/Calc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace calc.Model
+{
+    public class Calc
+    {
+        public float Add(float paramOne, float paramTwo)
+        { return (paramOne + paramTwo); }
+
+        public float Sub(float paramOne, float paramTwo)
+        { return (paramOne - paramTwo); }
+
+        public float Mul(float paramOne, float paramTwo)
+        { return (paramOne * paramTwo); }
+
+        public float Div(float paramOne, float paramTwo)
+        { return (paramOne / paramTwo); }
+
+    }
+}
